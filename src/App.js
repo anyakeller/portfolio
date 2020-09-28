82ae682df62a31e983853eb2fbd5c03020ba06d8
Anya L Keller <kelleranyal@gmail.com>
2020-09-28 19:11:34 -0400

    asdf

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Header from "./components/Header";
 
 function App() {
   return (
-    <Router basename="portfolio">
+    <Router basename={process.env.PUBLIC_URL}>
       <div className="App">
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
           <a className="navbar-brand" href="/">
