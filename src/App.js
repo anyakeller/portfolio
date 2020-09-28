7d79b238a27b5a10ef4ea8a2e07d23458d4acc6b
Anya L Keller <kelleranyal@gmail.com>
2020-09-28 19:21:37 -0400

    maybe I fixed it

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {BrowserRouter as Router, Switch, Route, NavLink} from "react-router-dom";
+import {BrowserRouter as Router, Switch, Route, Link, NavLink} from "react-router-dom";
 import {Home, About} from "./routes";
 import Header from "./components/Header";
 
@@ -7,9 +7,9 @@ function App() {
   return (<Router basename={`${process.env.PUBLIC_URL}/`}>
     <div className="App">
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a className="navbar-brand" href={`${process.env.PUBLIC_URL}/`}>
+        <Link className="navbar-brand" to="/">
           Anya Keller
-        </a>
+        </Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-collapse-content" aria-controls="navbar-collapse-content" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
