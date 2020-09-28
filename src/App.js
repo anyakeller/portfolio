2c06c63bea048e27004938cf67ca6b18c503e89d
Anya L Keller <kelleranyal@gmail.com>
2020-09-28 19:18:52 -0400

    apparently i didn't fix it

--- src/App.js
+++ src/App.js
@@ -1,64 +1,38 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  NavLink
-} from "react-router-dom";
-import { Home, About } from "./routes";
+import {BrowserRouter as Router, Switch, Route, NavLink} from "react-router-dom";
+import {Home, About} from "./routes";
 import Header from "./components/Header";
 
 function App() {
-  return (
-    <Router basename={process.env.PUBLIC_URL}>
-      <div className="App">
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-          <a className="navbar-brand" href="/">
-            Anya Keller
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbar-collapse-content"
-            aria-controls="navbar-collapse-content"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div
-            className="collapse navbar-collapse"
-            id="navbar-collapse-content"
-          >
-            <div className="navbar-nav">
-              <NavLink exact to="/" className="nav-link" activeClassName="active">
-                Home
-              </NavLink>
-              <NavLink
-                to="/about"
-                className="nav-link"
-                activeClassName="active"
-              >
-                About
-              </NavLink>
-            </div>
+  return (<Router basename={`${process.env.PUBLIC_URL}/`}>
+    <div className="App">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a className="navbar-brand" href={`${process.env.PUBLIC_URL}/`}>
+          Anya Keller
+        </a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-collapse-content" aria-controls="navbar-collapse-content" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbar-collapse-content">
+          <div className="navbar-nav">
+            <NavLink exact to="/" className="nav-link" activeClassName="active">
+              Home
+            </NavLink>
+            <NavLink to="/about" className="nav-link" activeClassName="active">
+              About
+            </NavLink>
           </div>
-        </nav>
-        <Header />
-        <div className="container">
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="/about">
-              <About />
-            </Route>
-          </Switch>
         </div>
+      </nav>
+      <Header/>
+      <div className="container">
+        <Switch>
+          <Route exact path="/" component={Home}></Route>
+          <Route path="/about" component={About}></Route>
+        </Switch>
       </div>
-    </Router>
-  );
+    </div>
+  </Router>);
 }
 
 export default App;
