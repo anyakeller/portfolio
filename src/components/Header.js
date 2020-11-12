57d340f94848e723dadd8bfbb1e9ec8420727330
Anya L Keller <kelleranyal@gmail.com>
2020-11-12 13:02:32 -0500

    fixed react router link problem

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React from "react";
-import {useLocation} from 'react-router-dom';
+import {useLocation, Link} from 'react-router-dom';
 
 function Header(props) {
   const location = useLocation();
   return (<header className="App-header px-2">
-    <a href={location.pathname}><small className="text-muted">{location.pathname}</small></a>
+    <Link to={location.pathname}>
+      <small className="text-muted">{location.pathname}</small>
+    </Link>
   </header>);
 }
 
