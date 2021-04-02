30cee20625f3c045d7a5ce8bcceb055b7d5456b4
Anya L. Keller <kelleranyal@gmail.com>
2021-04-01 22:27:55 -0400

    made nav a component

--- src/App.js
+++ src/App.js
@@ -1,67 +1,14 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  NavLink,
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Home, Portfolio, Contact } from "./routes";
+import Nav from "./components/Nav";
 import Header from "./components/Header";
 
 function App() {
   return (
     <Router basename={`${process.env.PUBLIC_URL}/`}>
       <div className="App">
-        <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
-          <Link className="navbar-brand" to="/">
-            Anya Keller
-          </Link>
-
-          <button
-            className="navbar-toggler hamburger collapsed hamburger--squeeze"
-            data-toggle="collapse"
-            type="button"
-            data-target="#navbar-collapse-content"
-            aria-controls="navbar-collapse-content"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="hamburger-box">
-              <span className="hamburger-inner"></span>
-            </span>
-          </button>
-
-          <div
-            className="collapse navbar-collapse"
-            id="navbar-collapse-content"
-          >
-            <div className="navbar-nav">
-              <NavLink
-                exact={true}
-                to="/"
-                className="nav-link"
-                activeClassName="active"
-              >
-                Home
-              </NavLink>
-              <NavLink
-                to="/portfolio"
-                className="nav-link"
-                activeClassName="active"
-              >
-                Portfolio
-              </NavLink>
-              <NavLink
-                to="/contact"
-                className="nav-link"
-                activeClassName="active"
-              >
-                Contact
-              </NavLink>
-            </div>
-          </div>
-        </nav>
+        <Nav />
         <Header />
         <div className="container pt-3 pb-2">
           <Switch>
---
+++ src/components/Nav.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+
+export default function Nav() {
+  return (
+    <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+      <Link className="navbar-brand" to="/">
+        Anya Keller
+      </Link>
+
+      <button
+        className="navbar-toggler hamburger collapsed hamburger--squeeze"
+        data-toggle="collapse"
+        type="button"
+        data-target="#navbar-collapse-content"
+        aria-controls="navbar-collapse-content"
+        aria-expanded="false"
+        aria-label="Toggle navigation">
+        <span className="hamburger-box">
+          <span className="hamburger-inner"></span>
+        </span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbar-collapse-content">
+        <div className="navbar-nav">
+          <NavLink
+            exact={true}
+            to="/"
+            className="nav-link"
+            activeClassName="active">
+            Home
+          </NavLink>
+          <NavLink
+            to="/portfolio"
+            className="nav-link"
+            activeClassName="active">
+            Portfolio
+          </NavLink>
+          <NavLink to="/contact" className="nav-link" activeClassName="active">
+            Contact
+          </NavLink>
+        </div>
+      </div>
+    </nav>
+  );
+}
