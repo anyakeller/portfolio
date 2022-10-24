ef2c724af6c41f1eeca78c4b905018c77e4c2793
Anya L. Keller <kelleranyal@gmail.com>
2022-10-24 12:03:31 -0400

    Updates

---
+++ precache-manifest.bfbe73d4d5d676c5968598cf0995f305.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd6c5958812b205ff4841f8aaafe6478",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "11acae337ee388a2b5d6",
+    "url": "/portfolio/static/css/main.3923b159.chunk.css"
+  },
+  {
+    "revision": "aba04ef2ec2f1ad5f211",
+    "url": "/portfolio/static/js/2.7a5a4675.chunk.js"
+  },
+  {
+    "revision": "b4cbc74a1d53cd16e2d14f8e29ca9a06",
+    "url": "/portfolio/static/js/2.7a5a4675.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11acae337ee388a2b5d6",
+    "url": "/portfolio/static/js/main.3c5c8d24.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.68d7e9050da921efffebee6ab080036b.js"
+  "/portfolio/precache-manifest.bfbe73d4d5d676c5968598cf0995f305.js"
 );
 
 self.addEventListener('message', (event) => {
