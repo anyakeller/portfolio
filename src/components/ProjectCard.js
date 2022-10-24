c56b3d6659bf07744ffea2f24b3eba1a8e957634
Anya L. Keller <kelleranyal@gmail.com>
2022-10-24 11:31:34 -0400

    Added kite and rocket website to projects

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,34 +1,47 @@
 import React from "react";
 
-function ProjectCard( props ) {
+function ProjectCard(props) {
   const projectData = props.projectData;
 
   function makeLinks() {
-    if ( projectData.links.length > 0 ) {
-      return projectData.links.map( ( link, id ) => {
-        return ( <a key={id} href={link.url}>{link.source}</a> );
-      } )
+    if (projectData.links.length > 0) {
+      return projectData.links.map((link, id) => {
+        return (
+          <a className="d-block" key={id} href={link.url}>
+            {link.source}
+          </a>
+        );
+      });
     }
   }
 
   function makeTags() {
-    if ( projectData.info.tags.length > 0 ) {
-      return projectData.info.tags.map( ( tag, id ) => {
-        return ( <span className="badge badge-pill badge-dark mr-1" key={id}>{tag}</span> );
-      } )
+    if (projectData.info.tags.length > 0) {
+      return projectData.info.tags.map((tag, id) => {
+        return (
+          <span className="badge badge-pill badge-dark mr-1" key={id}>
+            {tag}
+          </span>
+        );
+      });
     }
   }
-  return ( <div className="card shadow">
-    <img className="card-img-top" src={"project-media/" + projectData.img_file} alt="180x250 placeholder img"/>
-    <div className="card-body">
-      <h5 className="card-title">{projectData.project_name}</h5>
-      <p className="card-text">{projectData.info.description}</p>
-      {makeLinks()}
+  return (
+    <div className="card shadow">
+      <h5 class="card-header">{projectData.project_name}</h5>
+      <img
+        className="card-img-top"
+        src={"project-media/" + projectData.img_file}
+        alt="180x250 placeholder img"
+      />
+      <div className="card-body">
+        <small className="text-muted">{projectData.dates}</small>
+        <p className="card-text">{projectData.info.description}</p>
+        {makeLinks()}
+      </div>
+      <div className="card-footer">{makeTags()}</div>
     </div>
-    <div className="card-footer">
-      {makeTags()}
-    </div>
-  </div> );
+  );
 }
 
 export default ProjectCard;
