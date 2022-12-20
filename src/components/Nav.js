1ba601c958df5a37d7a4ba2d82c0db4ed2a24826
Anya L. Keller <kelleranyal@gmail.com>
2022-12-20 09:41:38 -0800

    fixed hamburgers and navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,9 +3,11 @@ import React from "react";
 export default function Nav() {
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
-      <a className="navbar-brand" href="/portfolio">
-        Anya Keller
-      </a>
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/portfolio">
+          Anya Keller
+        </a>
+      </div>
     </nav>
   );
 }
